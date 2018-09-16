ddd02f40c684ef5da23b19f69a61b77c44f7d7fe
Brennan Larson <brennan@SnekBook-Pro.local>
2018-09-16 13:16:19 -0700

    initial project

---
+++ app/src/main/java/com/github/brennanananan/camerafragments/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.brennanananan.camerafragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.brennanananan.camerafragments.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
